2c485415634cff1c39bb35f61446ab449d3a1c40
rasedul99 <rasedul2199@gmail.com>
2022-02-17 18:15:16 +0600

    getInputValue function added

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
+// get input value
+function getInputValue(id) {
+  const resultText = document.getElementById("id").value;
+  const result = parseInt(resultText);
+  return result;
+}
+
+// add event listener in calculation button
 document.getElementById("calculation").addEventListener("click", function () {
-  const incomeText = document.getElementById("income-input").value;
-  const foodText = document.getElementById("food-cost").value;
-  const rentText = document.getElementById("rent-cost").value;
-  const clothesText = document.getElementById("clothes-cost").value;
   const totalExpensesField = document.getElementById("total-expenses");
   const balanceField = document.getElementById("balance");
-
-  const income = parseFloat(incomeText);
-  const food = parseFloat(foodText);
-  const rent = parseFloat(rentText);
-  const clothes = parseFloat(clothesText);
+  const income = getInputValue("income-input");
+  const food = getInputValue("food-cost");
+  const rent = getInputValue("rent-cost");
+  const clothes = getInputValue("clothes-cost");
   const totalExpenses = food + rent + clothes;
   const balance = income - totalExpenses;
   totalExpensesField.innerText = totalExpenses;
   balanceField.innerText = balance;
-
-  console.log(totalExpenses);
-  console.log(balance);
 });
-
+// add event listener in savings button
 document
   .getElementById("savings-button")
   .addEventListener("click", function () {
-    const savingsInputText = document.getElementById("savings-input").value;
-    const savingsInput = parseFloat(savingsInputText);
+    const savingsInput = getInputValue("savings-input");
 
     const balanceFieldText = document.getElementById("balance").innerText;
     const balanceField = parseFloat(balanceFieldText);
