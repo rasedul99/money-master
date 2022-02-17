209018b1d5684800381b0a01fc2456a46b93e3a1
rasedul99 <rasedul2199@gmail.com>
2022-02-17 19:51:57 +0600

    update calcultaion

--- js/app.js
+++ js/app.js
@@ -5,15 +5,21 @@ function getInputValue(id) {
   return result;
 }
 
-// add event listener in calculation button
-document.getElementById("calculation").addEventListener("click", function () {
-  const totalExpensesField = document.getElementById("total-expenses");
-  const balanceField = document.getElementById("balance");
+function getTotalExpense() {
   const income = getInputValue("income-input");
   const food = getInputValue("food-cost");
   const rent = getInputValue("rent-cost");
   const clothes = getInputValue("clothes-cost");
   const totalExpenses = food + rent + clothes;
+  return totalExpenses;
+}
+
+// add event listener in calculation button
+document.getElementById("calculation").addEventListener("click", function () {
+  const totalExpensesField = document.getElementById("total-expenses");
+  const balanceField = document.getElementById("balance");
+  const totalExpenses = getTotalExpense();
+  const income = getInputValue("income-input");
   const balance = income - totalExpenses;
   totalExpensesField.innerText = totalExpenses;
   balanceField.innerText = balance;
@@ -24,10 +30,9 @@ document
   .addEventListener("click", function () {
     const savingAmount = getInputValue("savings-input");
     const income = getInputValue("income-input");
-
     const saving = (savingAmount / 100) * income;
-    const remainingBalance = income - saving;
-
+    const totalExpenses = getTotalExpense();
+    const remainingBalance = income - (saving + totalExpenses);
     const savingAmountFiled = document.getElementById("saving-amount");
     const remainingBalanceField = document.getElementById("remaining-balance");
 
